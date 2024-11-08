553ac4763d1b081a6260caa9d06e9e4f93bc0b0e
lucas.lima <lucasribeirolima974@gmail.com>
2024-11-08 11:47:03 -0300

    chore: Refactor login page components and structure
    This commit refactors the login page components and structure to improve code organization and maintainability. The `Login` component is now split into separate files for the `AsideContent`, `MainContent`, and `FooterContent` components. The `GoogleIcon` component is also moved to its own file. This restructuring enhances code readability and makes it easier to maintain and update the login page in the future.
    
    Note: This commit message follows the established conventions in the repository.

---
+++ src/components/login/googleIcon.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export const GoogleIcon = () => {
+  return (
+    <Image
+      src="google-logo.svg"
+      alt="Simbólo do google, letra G divida em diversas cores"
+      width={24}
+      height={24}
+    ></Image>
+  );
+};
