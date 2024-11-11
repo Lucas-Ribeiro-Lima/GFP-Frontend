2295fa1cb7081e1f5e3b6a54fa80ab60482a6c0a
lucas.lima <lucasribeirolima974@gmail.com>
2024-11-11 14:36:20 -0300

    feat: Add AuthProvider component for managing user authentication
    This commit adds the AuthProvider component, which manages user authentication in the application. It utilizes the provided userService to load user data and handle logoff functionality. The component also includes a useEffect hook to redirect users to the login page if the user object is null. This addition improves code organization and separates the authentication logic from other components.
    
    Note: This commit message follows the established conventions in the repository.

---
+++ src/contexts/authContext.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { AuthContextProps, ContaProps } from '@/domain/types'
+import { UserContract } from '@/services/userService'
+import { createContext, useEffect, useState } from 'react'
+
+export const AuthContext = createContext({} as AuthContextProps)
+
+export type AuthProviderProps = {
+  children: React.ReactNode
+  userService: UserContract
+} 
+
+export function AuthProvider({ children, userService }: AuthProviderProps) {
+  const [ user, setUser ] = useState<ContaProps | null>({} as ContaProps)
+
+  const logoff = async () => {
+    await userService.logout()
+    window.location.href = '/login'
+  }
+
+  useEffect(() => {
+    const loadUser = async () => {
+      const user = await userService.load()
+      setUser(user)
+    }
+    loadUser()
+    if (user === null) window.location.href = "/login"
+  }, [])
+
+  return (
+      <AuthContext.Provider value={{user, logoff}}>
+        {children}
+      </AuthContext.Provider>
+  )
+}
