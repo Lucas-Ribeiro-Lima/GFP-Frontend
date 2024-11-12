db42f51eca947dc38ea117bbf62a318edfc7d354
lucas.lima <lucasribeirolima974@gmail.com>
2024-11-12 13:34:42 -0300

    Refatoração: Ajustes nos componentes esqueletos das páginas

--- src/components/skeleton/layout.tsx
+++ src/components/skeleton/layout.tsx
@@ -5,7 +5,7 @@ export function LayoutSkeleton() {
     <div className="flex flex-col h-full p-4 bg-gradient-to-br from-white via-sky-100 to-white">
       <SkeletonComponents.Navbar/>
       <div className="flex flex-col flex-1">
-        <SkeletonComponents.Navbar/>
+        <SkeletonComponents.Header/>
         <main className="flex flex-col flex-1">
           <SkeletonComponents.Dashboard/>
           <SkeletonComponents.Base className="flex-1 m-8 p-4 rounded-sm bg-slate-300/60"/>
